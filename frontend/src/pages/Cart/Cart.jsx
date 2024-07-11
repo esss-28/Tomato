b69bcc981bfb5e03f4394b854019f2e2db6c5e09
Seher Siddiqui <163129625+esss-28@users.noreply.github.com>
2024-07-11 12:08:57 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../Context/StoreContext'
+import React, { useContext, useState } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../Context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-
-  const {cartItems, food_list, removeFromCart,getTotalCartAmount,url,currency,deliveryCharge} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url, currency, deliveryCharge } = useContext(StoreContext);
   const navigate = useNavigate();
+
+  const [promoCode, setPromoCode] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handlePromoCodeSubmit = () => {
+    // For simplicity, let's assume 'DISCOUNT10' is the only valid promo code
+    if (promoCode !== 'DISCOUNT10') {
+      setErrorMessage('Invalid promo code');
+    } else {
+      setErrorMessage('');
+      // Apply promo code logic here
+    }
+  };
 
   return (
     <div className='cart'>
@@ -17,19 +29,20 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id]>0) {
-            return (<div key={index}>
-              <div className="cart-items-title cart-items-item">
-                {/* <img src={url+"/images/"+item.image} alt="" /> */}
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{currency}{item.price}</p>
-                <div>{cartItems[item._id]}</div>
-                <p>{currency}{item.price*cartItems[item._id]}</p>
-                <p className='cart-items-remove-icon' onClick={()=>removeFromCart(item._id)}>x</p>
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{currency}{item.price}</p>
+                  <div>{cartItems[item._id]}</div>
+                  <p>{currency}{item.price * cartItems[item._id]}</p>
+                  <p className='cart-items-remove-icon' onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
               </div>
-              <hr />
-            </div>)
+            );
           }
         })}
       </div>
@@ -39,26 +52,32 @@ const Cart = () => {
           <div>
             <div className="cart-total-details"><p>Subtotal</p><p>{currency}{getTotalCartAmount()}</p></div>
             <hr />
-            <div className="cart-total-details"><p>Delivery Fee</p><p>{currency}{getTotalCartAmount()===0?0:deliveryCharge}</p></div>
+            <div className="cart-total-details"><p>Delivery Fee</p><p>{currency}{getTotalCartAmount() === 0 ? 0 : deliveryCharge}</p></div>
             <hr />
-            <div className="cart-total-details"><p>Taxes</p><p>{currency}{getTotalCartAmount()===0?0:deliveryCharge}</p></div>
+            <div className="cart-total-details"><p>Taxes</p><p>{currency}{getTotalCartAmount() === 0 ? 0 : deliveryCharge}</p></div>
             <hr />
-            <div className="cart-total-details"><b>Total</b><b>{currency}{getTotalCartAmount()===0?0:getTotalCartAmount()+deliveryCharge}</b></div>
+            <div className="cart-total-details"><b>Total</b><b>{currency}{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + deliveryCharge}</b></div>
           </div>
-          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
             <div className='cart-promocode-input'>
-              <input type="text" placeholder='promo code'/>
-              <button>Submit</button>
+              <input
+                type="text"
+                placeholder='promo code'
+                value={promoCode}
+                onChange={(e) => setPromoCode(e.target.value)}
+              />
+              <button onClick={handlePromoCodeSubmit}>Submit</button>
             </div>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
